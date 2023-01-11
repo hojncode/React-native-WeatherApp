5e9ca47c17159ad36e4d5978c55c9ab0efe51700
hojncode <glicm12@gmail.com>
2023-01-12 01:01:48 +0900

    StyleSheet - style 작성 룰, StatusBar 의 역할

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>hello native - hojncode!!</Text>
+      <Text style={styles.text}>hello native - hojncode!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "tomato",
     alignItems: "center",
     justifyContent: "center",
   },
+  text: {
+    fontSize: 28,
+    color: "#40739e",
+  },
 });
+
+//StyleSheet.create 는 단지 object 로 사용 안해도 구동되지만, 자동완성 기능을 지원해주기에 사용한다. (인라인으로 css 작성도 가능하다.)
